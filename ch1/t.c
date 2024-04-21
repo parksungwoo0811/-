69266afb5a2710d9ae47d9e68ec0c03047beb465
parksungwoo0811 <127467004+parksungwoo0811@users.noreply.github.com>
2024-04-22 02:08:36 +0900

    Update t.c

--- ch1/t.c
+++ ch1/t.c
@@ -3,11 +3,14 @@
 
 int main() {
     char *ptr, *new;
-    ptr = malloc(sizeof(char) * 100);
-/*
-    ptr = calloc(10, 20);
-*/
-    new = realloc(ptr, 100);
+    ptr = malloc(sizeof(char) * 100); // 메모리 할당
 
-    printf("addr: %p\n", ptr);
+    // calloc 함수를 사용하려면 아래 주석을 해제하세요.
+    /*
+    ptr = calloc(10, 20); // 10개의 요소를 가진 배열을 20바이트씩 할당
+    */
+
+    new = realloc(ptr, 100); // 메모리 다시 할당
+
+    printf("addr: %p\n", ptr); // 할당된 메모리의 주소 출력
 }
